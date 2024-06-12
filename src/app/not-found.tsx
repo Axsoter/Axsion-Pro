a884677562d053f3773c4b6575cdc344c0d0a6bc
Nino K <helsincraft@gmail.com>
2024-06-12 17:45:33 +0300

    added navlogin button

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,7 +5,7 @@ export default function NotFound() {
         <section className="min-h-screen flex items-center justify-center">
             <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
                 <div className="mx-auto max-w-screen-sm text-center">
-                    <h1 className="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-axsoterBlue">404</h1>
+                    <h1 className="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-axsoterBlue">4<Link href="https://www.youtube.com/watch?v=jeg_TJvkSjg">0</Link>4</h1>
                     <p className="mb-4 text-3xl tracking-tight font-bold md:text-4xl text-white">Täältäpä ei löytynytkään mitään.</p>
                     <p className="mb-4 text-lg font-light text-gray-400">Sivua mitä etsit ei löydy ainakaan täältä.</p>
                     <Link href="/" className="inline-flex text-white bg-axsoterBlue ease-in-out duration-700 hover:bg-brandGradient font-medium rounded-lg text-sm px-5 py-2.5 text-center my-4">Takaisin kotisivuille</Link>
